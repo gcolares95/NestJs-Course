b74411957090b40958d954ded560577f29fdd0b7
Guilherme Colares De Araujo Da Silva <guilherme.araujo@sidia.com>
2024-05-17 20:00:04 -0400

    .

--- src/usuario/usuario.repository.ts
+++ src/usuario/usuario.repository.ts
@@ -1,9 +1,14 @@
+import { Injectable } from "@nestjs/common";
+
+@Injectable()
 export class UsuarioRepository {
     private usuarios = [];
 
     async salvar(usuario) {
         this.usuarios.push(usuario);
-
-        console.log(this.usuarios);
+    }
+    
+    async listar() {
+        return this.usuarios;
     }
 }
